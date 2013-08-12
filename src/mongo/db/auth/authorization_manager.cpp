24e3667c836da8adf52e966dde9bb518a69ff31a
Spencer T Brody <spencer@10gen.com>
2013-08-12 18:18:08 -0400

    SERVER-9518 Reload User if User has been invalidated

--- src/mongo/db/auth/authorization_manager.cpp
+++ src/mongo/db/auth/authorization_manager.cpp
@@ -731,6 +731,15 @@ namespace {
         }
     }
 
+    void AuthorizationManager::invalidateUser(User* user) {
+        boost::lock_guard<boost::mutex> lk(_lock);
+        unordered_map<UserName, User*>::iterator it = _userCache.find(user->getName());
+        if (it != _userCache.end() && it->second == user) {
+            _userCache.erase(it);
+        }
+        user->invalidate();
+    }
+
     void AuthorizationManager::addInternalUser(User* user) {
         boost::lock_guard<boost::mutex> lk(_lock);
         _userCache.insert(make_pair(user->getName(), user));
