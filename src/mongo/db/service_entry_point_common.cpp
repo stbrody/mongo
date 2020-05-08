df93eb5fc23a02bd951fd63dde9bcdd4e3437ca8
Spencer T Brody <spencer@mongodb.com>
2020-05-08 23:55:01 +0000

    stop logging every received command

--- src/mongo/db/service_entry_point_common.cpp
+++ src/mongo/db/service_entry_point_common.cpp
@@ -934,7 +934,6 @@ void execCommandDatabase(OperationContext* opCtx,
                          const OpMsgRequest& request,
                          rpc::ReplyBuilderInterface* replyBuilder,
                          const ServiceEntryPointCommon::Hooks& behaviors) {
-    logd("BBBBBBBBBBBB Received command: {}", request.body);
     CommandHelpers::uassertShouldAttemptParse(opCtx, command, request);
     BSONObjBuilder extraFieldsBuilder;
     auto startOperationTime = getClientOperationTime(opCtx);
